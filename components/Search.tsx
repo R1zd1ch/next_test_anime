9e607d8dbd649d6fcc8588a8d1997efe8b8d5158
rizzd <evgeniy_cheraev@vk.com>
2024-10-18 14:32:54 +0300

    1

--- components/Search.tsx
+++ components/Search.tsx
@@ -11,7 +11,7 @@ const Search = ({ onFocus }: { onFocus: () => void }) => {
       <input
         type="text"
         placeholder="Поиск..."
-        className="w-full px-4 py-2 text-sm lg:text-base rounded-lg border border-neutral-50 bg-transparent focus:outline-none focus:ring-2 focus:ring-neutral-400"
+        className="w-full px-4 py-2 text-base lg:text-base rounded-lg border border-neutral-50 bg-transparent focus:outline-none focus:ring-2 focus:ring-neutral-400"
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)} // Обновляем Zustand состояние
         onFocus={onFocus} // Вызов, когда поле получает фокус
